39b6485f2645f72dbea9c60f94a898e9a02e97f0
Ivana Pusic <pusicka97@gmail.com>
2023-10-03 10:26:22 +0200

    add: create components

---
+++ src/components/PetFacts.jsx
@@ -0,0 +1,5 @@
+const PetFacts = () => {
+  return <div>PetFacts</div>;
+};
+
+export default PetFacts;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Footer from './Footer';
+import Form from './Form';
+import Gallery from './Gallery';
+import Hero from './Hero';
+import PetFacts from './PetFacts';
+
+export { Footer, Form, Gallery, Hero, PetFacts };
