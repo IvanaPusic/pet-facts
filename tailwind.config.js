a6c3156ff507bd5b2c0e05f5f5f578fccfecee83
Ivana Pusic <pusicka97@gmail.com>
2023-10-03 11:02:06 +0200

    add: setup theme in  tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f7f7f7',
+        secondary: '#2b2b2b',
+        dark: '#1d1d1d',
+      },
+    },
+    fontSize: {
+      '4xl': '3.625rem',
+      '3xl': '3.25rem',
+      '2xl': '2.875rem',
+      xl: '2.25rem',
+      large: '1.25rem',
+      base: '1.125rem',
+      small: '1.10804rem',
+      xs: '1rem',
+    },
   },
   plugins: [],
 };
