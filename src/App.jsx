f378681ce5b6234e1c8e328e553206a03041d34f
Ivana Pusic <pusicka97@gmail.com>
2023-10-03 10:27:13 +0200

    add: import components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Footer, Form, Gallery, Hero, PetFacts } from './components';
+
 function App() {
   return (
     <>
-      <h1 className='text-3xl bold uppercase'>Pet facts</h1>
+      <Hero />
+      <PetFacts />
+      <Gallery />
+      <Form />
+      <Footer />
     </>
   );
 }
