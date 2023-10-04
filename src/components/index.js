35691421e37affeb82e163451d1063860f204b95
Ivana Pusic <pusicka97@gmail.com>
2023-10-04 22:14:16 +0200

    add: import swiper next button and swiper previous button

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,15 @@ import Form from './Form';
 import Gallery from './Gallery';
 import Hero from './Hero';
 import PetFacts from './PetFacts';
+import SwiperButtonNext from './SwiperButtonNext';
+import SwiperButtonPrev from './SwiperButtonPrev';
 
-export { Footer, Form, Gallery, Hero, PetFacts };
+export {
+  Footer,
+  Form,
+  Gallery,
+  Hero,
+  PetFacts,
+  SwiperButtonNext,
+  SwiperButtonPrev,
+};
