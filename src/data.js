b903941e759f7509b9070f40610add667d2529d1
Ivana Pusic <pusicka97@gmail.com>
2023-10-04 21:19:41 +0200

    add: create data.js file

---
+++ src/data.js
@@ -0,0 +1,42 @@
+import img2 from './assets/gallery-img.png';
+import img3 from './assets/gallery-img2.png';
+import img1 from './assets/hot-dog.png';
+
+export const gallery = [
+  {
+    id: 1,
+    img: img1,
+  },
+  {
+    id: 2,
+    img: img2,
+  },
+  {
+    id: 3,
+    img: img3,
+  },
+  {
+    id: 4,
+    img: img1,
+  },
+  {
+    id: 5,
+    img: img2,
+  },
+  {
+    id: 6,
+    img: img3,
+  },
+  {
+    id: 7,
+    img: img1,
+  },
+  {
+    id: 8,
+    img: img2,
+  },
+  {
+    id: 9,
+    img: img3,
+  },
+];
