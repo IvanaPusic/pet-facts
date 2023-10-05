e7ace298a04b6170b9745abe00d8f0427f5b7928
Ivana Pusic <pusicka97@gmail.com>
2023-10-05 13:18:13 +0200

    add: add mobile and desktop images to array

--- src/data.js
+++ src/data.js
@@ -2,6 +2,26 @@ import img2 from './assets/gallery-img.png';
 import img3 from './assets/gallery-img2.png';
 import img1 from './assets/hot-dog.png';
 
+// mobile gallery
+import catMobile1 from './assets/cat-mobile1.png';
+import catMobile2 from './assets/cat-mobile2.png';
+import catMobile3 from './assets/cat-mobile3.png';
+import catMobile4 from './assets/cat-mobile4.png';
+import catMobile5 from './assets/cat-mobile5.png';
+import catMobile6 from './assets/cat-mobile6.png';
+import catMobile7 from './assets/cat-mobile7.png';
+import catMobile8 from './assets/cat-mobile8.png';
+
+// desktop
+import cat1 from './assets/cat1.png';
+import cat2 from './assets/cat2.png';
+import cat3 from './assets/cat3.png';
+import cat4 from './assets/cat4.png';
+import cat5 from './assets/cat5.png';
+import cat6 from './assets/cat6.png';
+import cat7 from './assets/cat7.png';
+import cat8 from './assets/cat8.png';
+
 export const gallery = [
   {
     id: 1,
@@ -26,5 +46,75 @@ export const gallery = [
   {
     id: 6,
     img: img3,
+  },
+];
+
+export const mobileGallery = [
+  {
+    id: 1,
+    img: catMobile1,
+  },
+  {
+    id: 2,
+    img: catMobile2,
+  },
+  {
+    id: 3,
+    img: catMobile3,
+  },
+  {
+    id: 4,
+    img: catMobile4,
+  },
+  {
+    id: 5,
+    img: catMobile5,
+  },
+  {
+    id: 6,
+    img: catMobile6,
+  },
+  {
+    id: 7,
+    img: catMobile7,
+  },
+  {
+    id: 8,
+    img: catMobile8,
+  },
+];
+
+export const desktopGallery = [
+  {
+    id: 1,
+    img: cat1,
+  },
+  {
+    id: 2,
+    img: cat2,
+  },
+  {
+    id: 3,
+    img: cat3,
+  },
+  {
+    id: 4,
+    img: cat4,
+  },
+  {
+    id: 5,
+    img: cat5,
+  },
+  {
+    id: 6,
+    img: cat6,
+  },
+  {
+    id: 7,
+    img: cat7,
+  },
+  {
+    id: 8,
+    img: cat8,
   },
 ];
